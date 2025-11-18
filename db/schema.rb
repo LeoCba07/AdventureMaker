d6ce2c8556e8ed95619dbc2b6c51ad9ff7f638af
Haley Banfield <HaleyKBanfield@gmail.com>
2025-11-18 12:15:23 +0900

    test of failsafe for pushes

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# My name is Hales, and this is a test of the failsafe master branch push protection
+
 ActiveRecord::Schema[7.1].define(version: 2025_11_17_080050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
