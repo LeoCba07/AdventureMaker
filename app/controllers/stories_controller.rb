6a587962de72b3edd0d297609445f874095abdfe
Haley Banfield <HaleyKBanfield@gmail.com>
2025-11-18 18:03:00 +0900

    Figured out the ordering and initialization -- currently properly redirects to the story page with the hard-coded message

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,12 +4,17 @@ class StoriesController < ApplicationController
   end
 
   def create
+    # initialize the story and chat
     @story = Story.new(story_params)
     @chat = Chat.new
+
+    # set up the chat for transfer
     @chat.story = @story
     @story.user = current_user
-    if @story.save
 
+    if @story.save
+      # 11/18 -- this is the line we need to change. We need to give the AI the story and make this line the AI's
+      @chat.messages.create!(role: "system", content: "This is a fucking test, bro")
       redirect_to chat_path(@chat), notice: "A new story has begun!"
     else
       render :new, status: :unprocessable_entity
