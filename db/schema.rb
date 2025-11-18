4d23f2e0426b33a980a642dcad8591d5ad8212f2
Haley Banfield <HaleyKBanfield@gmail.com>
2025-11-18 15:15:30 +0900

    Added the foundation of the story creation form as well as a capitalization for form labels in the application.scss file

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# My name is Hales, and this is a test of the failsafe master branch push protection
-
 ActiveRecord::Schema[7.1].define(version: 2025_11_17_080050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
